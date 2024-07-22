5ad6e0785615e4abf4e4b68c16a86c67bc5b7263
北條佑弥 <y.hojo@core-tech.jp>
2024-07-23 00:09:24 +0900

    ルーティングテスト

--- example.go
+++ example.go
@@ -13,5 +13,12 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	// This handler will match /user/john but will not match /user/ or /user
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
